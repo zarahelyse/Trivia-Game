8bf595d1fd4d04564994ef89b47d0d9f0931466c
Zarah Parker <parker@civicplus.com>
2018-03-15 20:57:29 -0400

    1.0

---
+++ assets/javascript/app.js
@@ -0,0 +1,126 @@
+$(document).ready(function() {
+
+//Plays Star Wars theme song
+
+$('#audio')[0].play();
+
+// Create a function that creates the start button and initial screen
+
+function initialScreen() {
+	startScreen = "<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>May the force be with you!</a></p>";
+	$(".mainArea").html(startScreen);
+}
+
+initialScreen();
+
+//Create a function, generateHTML(), that is triggered by the start button, and generates the HTML seen on the project video...
+
+$("body").on("click", ".start-button", function(event){
+	event.preventDefault();  // added line to test issue on GitHub Viewer
+	generateHTML();
+
+	timerWrapper();
+
+}); // Closes start-button click
+
+$("body").on("click", ".answer", function(event){
+	//answeredQuestion = true;
+	selectedAnswer = $(this).text();
+	if(selectedAnswer === correctAnswers[questionCounter]) {
+		//alert("correct");
+
+		clearInterval(theClock);
+		generateWin();
+	}
+	else {
+		//alert("wrong answer!");
+		clearInterval(theClock);
+		generateLoss();
+	}
+}); // Close .answer click
+
+$("body").on("click", ".reset-button", function(event){
+	resetGame();
+}); // Closes reset-button click
+
+});  //  Closes jQuery wrapper
+
+function generateLossDueToTimeOut() {
+	unansweredTally++;
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " + correctAnswers[questionCounter];
+	$(".mainArea").html(gameHTML);
+	setTimeout(wait, 4000);  //  change to 4000 or other amount
+}
+
+function generateWin() {
+	correctTally++;
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + correctAnswers[questionCounter];
+	$(".mainArea").html(gameHTML);
+	setTimeout(wait, 4000);  //  change to 4000 or other amount
+}
+
+function generateLoss() {
+	incorrectTally++;
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: "+ correctAnswers[questionCounter];
+	$(".mainArea").html(gameHTML);
+	setTimeout(wait, 4000); //  change to 4000 or other amount
+}
+
+function generateHTML() {
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" + questionArray[questionCounter] + "</p><p class='first-answer answer'>A. " + answerArray[questionCounter][0] + "</p><p class='answer'>B. "+answerArray[questionCounter][1]+"</p><p class='answer'>C. "+answerArray[questionCounter][2]+"</p><p class='answer'>D. "+answerArray[questionCounter][3]+"</p>";
+	$(".mainArea").html(gameHTML);
+}
+
+function wait() {
+	if (questionCounter < 7) {
+	questionCounter++;
+	generateHTML();
+	counter = 30;
+	timerWrapper();
+	}
+	else {
+		finalScreen();
+	}
+}
+
+function timerWrapper() {
+	theClock = setInterval(thirtySeconds, 1000);
+	function thirtySeconds() {
+		if (counter === 0) {
+			clearInterval(theClock);
+			generateLossDueToTimeOut();
+		}
+		if (counter > 0) {
+			counter--;
+		}
+		$(".timer").html(counter);
+	}
+}
+
+function finalScreen() {
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>All done, here's how you did!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + correctTally + "</p>" + "<p>Wrong Answers: " + incorrectTally + "</p>" + "<p>Unanswered: " + unansweredTally + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>";
+	$(".mainArea").html(gameHTML);
+}
+
+function resetGame() {
+	questionCounter = 0;
+	correctTally = 0;
+	incorrectTally = 0;
+	unansweredTally = 0;
+	counter = 30;
+	generateHTML();
+	timerWrapper();
+}
+
+var startScreen;
+var gameHTML;
+var counter = 30;
+var questionArray = ["Who killed Palpatine?", "What color is Obi-Wan's lightsaber?", "Who tells Luke, 'Join me and I will complete your training'?", "What planet is Anakin from?", "What color is Rey's lightsaber?", "What planet does Rey find Luke hiding out on?", "What is Leia's final title?", "Who does Luke say his final good-bye to?"];
+var answerArray = [["Han Solo", "Leia", "Darth Vader", "Luke"], ["Red","Blue","Purple","Green"], ["Yoda", "Obi-Wan", "Darth Vader","Emperor"], ["Hoth","Endor","Tatooine","Naboo"], ["Purple", "Red", "Green", "Blue"], ["Ahch-To","Endor","Bespin","Alderaan"], ["Princess", "General", "Queen", "Emperor"], ["Rey","Kylo Ren","Han Solo","Leia"]];
+var correctAnswers = ["A. Han Solo", "B. Blue", "C. Darth Vader", "C. Tatooine", "D. Blue", "A. Ahch-To", "B. General", "D. Leia"];
+var questionCounter = 0;
+var selecterAnswer;
+var theClock;
+var correctTally = 0;
+var incorrectTally = 0;
+var unansweredTally = 0;
